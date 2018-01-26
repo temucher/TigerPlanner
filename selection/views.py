f1e09d34ab784aeb66e8d9f36688f4e3bab7313b
Teagan Mucher <tjsmuch@gmail.com>
2018-01-26 11:23:49 -0800

    Added more html and css

--- selection/views.py
+++ selection/views.py
@@ -3,6 +3,7 @@ from .models import SubjectDropdown
 from .forms import SubjectForm
 from django.views.generic import FormView, CreateView
 
+# may end up needing a new view for a results page?
 
 def home(request):
     # template_name = 'select_class.html'
